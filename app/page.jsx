f7bcf556cc51e4a7bc917202e0fde61575741605
MuhammadTanveerS <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2023-08-21 13:11:27 +0500

    tweaks

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,9 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
             Discover & Share
-            <br className='max-md:hidden' />
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            {/* <br className='max-md:hidden' /> */}
+            <br />
+            <span className="blue_gradient text-center">AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
             Promptii is an open-source AI prompting tool for modern world to discover, create and share prompts.
